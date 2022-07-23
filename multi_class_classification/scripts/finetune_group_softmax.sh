8a75f1fefb0df6edfd7c1946476e1568b692091e
Radhika Dua <radhikadua1997@gmail.com>
2022-07-23 22:26:47 +0900

    Update finetune_group_softmax.sh

--- multi_class_classification/scripts/finetune_group_softmax.sh
+++ multi_class_classification/scripts/finetune_group_softmax.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+dataset_path=$1
 MODEL=BiT-S-R101x1
 
-CUDA_VISIBLE_DEVICES=2,3 python finetune.py \
+CUDA_VISIBLE_DEVICES=1 python finetune.py \
 --name finetune_group_softmax_${MODEL} \
 --model ${MODEL} \
 --bit_pretrained_dir bit_pretrained_models \
---logdir /home/radhika/mos1/checkpoints/finetune \
+--logdir checkpoints/finetune \
 --dataset imagenet2012 \
---eval_every 10 \
---datadir /home/data_storage/imagenet/v12 \
+--eval_every 200 \
+--datadir ${dataset_path} \
 --train_list data_lists/imagenet2012_group_softmax_train_list.txt \
 --val_list data_lists/imagenet2012_group_softmax_val_list.txt \
 --num_block_open 0 \
