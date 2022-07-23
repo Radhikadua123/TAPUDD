c8003638a99108a89447710ec5f0e2c329a44fd5
Radhika Dua <radhikadua1997@gmail.com>
2022-07-23 22:26:23 +0900

    Update finetune_flat_softmax.sh

--- multi_class_classification/scripts/finetune_flat_softmax.sh
+++ multi_class_classification/scripts/finetune_flat_softmax.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+dataset_path=$1
 MODEL=BiT-S-R101x1
 
-python finetune.py \
+CUDA_VISIBLE_DEVICES=0 python finetune.py \
 --name finetune_flat_softmax_${MODEL} \
 --model ${MODEL} \
 --bit_pretrained_dir bit_pretrained_models \
 --logdir checkpoints/finetune \
 --dataset imagenet2012 \
 --eval_every 200 \
---datadir dataset/id_data/ILSVRC-2012 \
+--datadir ${dataset_path} \
 --train_list data_lists/imagenet2012_train_list.txt \
 --val_list data_lists/imagenet2012_val_list.txt \
 --num_block_open 0 \
