06cb5977111a623e35b47b91d8d9cd648073faf4
Radhika Dua <radhikadua1997@gmail.com>
2022-08-09 21:16:31 +0900

    Update get_penultimate_features.sh

--- binary_classification/scripts/get_penultimate_features.sh
+++ binary_classification/scripts/get_penultimate_features.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-device=0
-data_path="/home/data_storage/radhika/data/datasets/boneage_data_kaggle/"
+device=2
+data_path=$1
 
-path="./results_ce"
-for inp_seed in 4 1955; do
+path="./results"
+for inp_seed in 0 1 2 663 1458 1708; do
     CUDA_VISIBLE_DEVICES=${device} python get_features.py --seed ${inp_seed} --flag_adjust --variation 'bright'  --result_path $path --data_path $data_path
 done
