91e15a2ca7882ec92f51fdcc9722b12eb17172d3
Radhika Dua <radhikadua1997@gmail.com>
2022-08-09 20:57:45 +0900

    Add changes to create log file for tune mahalanobis

--- binary_classification/utils_test/log.py
+++ binary_classification/utils_test/log.py
@@ -41,3 +41,43 @@ def setup_logger(args):
     logger.flush = lambda: [h.flush() for h in logger.handlers]
     logger.info(args)
     return logger
+
+def setup_tune_mahala_logger(args):
+    """Creates and returns a fancy logger."""
+    # return logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
+    os.makedirs(os.path.join(args.logdir, "seed_" + str(args.seed)), exist_ok=True)
+    logging.config.dictConfig({
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "standard": {
+                "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+            },
+        },
+        "handlers": {
+            "stderr": {
+                "level": "INFO",
+                "formatter": "standard",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stderr",
+            },
+            "logfile": {
+                "level": "DEBUG",
+                "formatter": "standard",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(args.logdir, "seed_" + str(args.seed), "tune_mahala.log"),
+                "mode": "a",
+            }
+        },
+        "loggers": {
+            "": {
+                "handlers": ["stderr", "logfile"],
+                "level": "DEBUG",
+                "propagate": True
+            },
+        }
+    })
+    logger = logging.getLogger(__name__)
+    logger.flush = lambda: [h.flush() for h in logger.handlers]
+    logger.info(args)
+    return logger
