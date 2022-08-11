539f463418113c022dfb276201e1e0a394754a33
Radhika Dua <radhikadua1997@gmail.com>
2022-08-11 18:10:03 +0900

    Update get_penultimate_features.sh

--- binary_classification/scripts/get_penultimate_features.sh
+++ binary_classification/scripts/get_penultimate_features.sh
@@ -4,6 +4,6 @@ device=2
 data_path=$1
 
 path="./results"
-for inp_seed in 0 1 2 663 1458 1708; do
+for inp_seed in 0 1 2 3 4 663 1458 1708 1955 7130; do
     CUDA_VISIBLE_DEVICES=${device} python get_features.py --seed ${inp_seed} --flag_adjust --variation 'bright'  --result_path $path --data_path $data_path
 done
