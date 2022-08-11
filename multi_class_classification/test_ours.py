0e9d7a5c3c275799fc07dc53440b5de7145f9ccf
Radhika Dua <radhikadua1997@gmail.com>
2022-08-11 18:41:52 +0900

    Update test_ours.py

--- multi_class_classification/test_ours.py
+++ multi_class_classification/test_ours.py
@@ -1,40 +1,41 @@
 import os
 import cv2
-import torch
+import time
 import random
 import pickle
+import argparse
 import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+import torchvision as tv
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import sklearn.metrics as sk
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 from sklearn.metrics import roc_auc_score
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
-import torchvision.transforms as transforms
+
 CUDA_LAUNCH_BLOCKING=1
 
-from torch.utils.data import Dataset
-import argparse
-import torchvision as tv
-import torch
-import numpy as np
-import sklearn.metrics as sk
-from utils.mahalanobis_ours import *
-import resnetv2
-from utils import log
-from utils.test_utils import arg_parser
-from utils.test_utils import stable_cumsum, fpr_and_fdr_at_recall, get_measures, plot_aupr_auroc
-from tune_mahalanobis_hyperparameter import mktrainval
-import torch.nn as nn
-import torchvision.models as models
-import torch.optim as optim
-import time
+from utils import *
+from dataset import *
+from utils_test.mahalanobis_ours import *
+from utils_test import log
+from utils_test.test_utils import arg_parser
+from utils_test.test_utils import stable_cumsum, fpr_and_fdr_at_recall, get_measures, plot_aupr_auroc
 
+torch.set_num_threads(1)
     
 def mk_id_ood(ood_feats, in_feats):
     """Returns train and validation datasets."""
@@ -44,10 +45,10 @@ def mk_id_ood(ood_feats, in_feats):
     print(f"Using an out-of-distribution set with {len(ood_set)} images.")
 
     in_loader = torch.utils.data.DataLoader(
-        in_set, batch_size=64, shuffle=False,
+        in_set, batch_size=256, shuffle=False,
         num_workers=4, pin_memory=True, drop_last=False)
     out_loader = torch.utils.data.DataLoader(
-        ood_set, batch_size=64, shuffle=False,
+        ood_set, batch_size=256, shuffle=False,
         num_workers=4, pin_memory=True, drop_last=False)
     
     return in_loader, out_loader
@@ -90,29 +91,12 @@ class Dataset_ood_test(Dataset):
 
 
 
-def run_eval(logger, model, args, estimator, num_groups, clustering_type, covar_type, use_gmm_stats, in_loader, out_loader, mean, var, device, metric = 'none'):
+def run_eval(logger, model, args, estimator, num_groups, clustering_type, covar_type, use_gmm_stats, train_loader, in_loader, out_loader, mean, var, device, metric = 'none'):
     # switch to evaluate mode
     model.eval()
     logger.info("Running test...")
     logger.flush()
     
-    if metric == 'mahalanobis':
-        #### get feature list of model used for getting mahala stats #######
-        # set information about feature extaction
-        temp_x = torch.rand(2, 3, 480, 480)
-        temp_x = Variable(temp_x).cuda()
-        temp_list = [model(x=temp_x, layer_index=4)[1]]
-        num_output = len(temp_list)
-        feature_list = np.empty(num_output)
-        count = 0
-        for out in temp_list:
-            feature_list[count] = out.size(1)
-            count += 1
-        feature_list = feature_list[0]
-        
-        if(clustering_type != "gaussian"):
-            mean, var = sample_estimator(model, num_classes=num_groups, feature_list= feature_list, train_loader = in_loader, device = device) ###not used ..
-        
     model.eval()
     out_score =[]
     in_score = []
@@ -126,17 +110,21 @@ def run_eval(logger, model, args, estimator, num_groups, clustering_type, covar_
         in_score.extend(in_confs)
     in_examples = np.array(in_score).reshape((-1, 1))
     out_examples = np.array(out_score).reshape((-1, 1))
-    dir_path = os.path.join(args.logdir, args.name)
+
+    dir_path = os.path.join(args.logdir, args.name, args.adjust_scale)
     os.makedirs(dir_path, exist_ok=True)
-    file_path_ood_scores = os.path.join(args.logdir, args.name, "ood_scores.npy")
+    file_path_ood_scores = os.path.join(args.logdir, args.name, args.adjust_scale, "ood_scores.npy")
+    dir_path = os.path.join(args.logdir, args.name, "1.0")
+    os.makedirs(dir_path, exist_ok=True)
+    file_path_id_scores = os.path.join(args.logdir, args.name, "1.0", "ood_scores.npy")
+
+
+    np.save(file_path_id_scores, in_examples)
     np.save(file_path_ood_scores, out_examples)
 
-    file_path_id_scores = os.path.join(args.logdir, args.name, "id_scores.npy")
-    np.save(file_path_id_scores, in_examples)
-
     auroc, aupr_in, aupr_out, fpr95 = get_measures(in_examples, out_examples)
-    os.makedirs(os.path.join(args.logdir, args.name), exist_ok=True)
-    path_auroc_aupr = os.path.join(args.logdir, args.name)
+    os.makedirs(os.path.join(args.logdir, args.name, args.adjust_scale), exist_ok=True)
+    path_auroc_aupr = os.path.join(args.logdir, args.name, args.adjust_scale)
     auc_roc, auc_aupr_in, auc_aupr_out = plot_aupr_auroc(in_examples, out_examples, path_auroc_aupr)
 
     logger.info('=================== Results for GMM + Mahala =================')
@@ -152,17 +140,26 @@ def run_eval(logger, model, args, estimator, num_groups, clustering_type, covar_
 
 
 
-def unsupervised_ood_detection(model, logger, args, num_of_clusters, clustering_type, covar_type, use_gmm_stats, ood_path, metric, device):
+def unsupervised_ood_detection(model, logger, args, num_of_clusters, clustering_type, covar_type, use_gmm_stats, ood_path, id_path, metric, device):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+    
+    train_feats_df = []
+    np.random.seed(1)
+    train_feats = np.load(args.train_feats_path)
+    train_feats_df.extend(train_feats)
+
+    train_Df = pd.DataFrame(list(zip(train_feats_df)), columns=["feats"])
+    
+    print(len(train_Df))
     
     # Apply clustering
     if(clustering_type == "kmeans"):
         estimator = KMeans(n_clusters = num_of_clusters)
     else:
-        estimator = GaussianMixture(n_components=num_of_clusters, covariance_type=covar_type, random_state = 0)
+        estimator = GaussianMixture(n_components=num_of_clusters, covariance_type=covar_type, random_state = args.gmm_random_state)
     
-    clustering_model_path = f'/home/radhika/mos1/checkpoints/clustering_models/imagenet/num_samples_{args.num_of_samples}'
+    clustering_model_path = args.clustering_model_path
     os.makedirs(clustering_model_path, exist_ok=True)
     clustering_model_path_file = os.path.join(clustering_model_path, f'estimator_cluster_{num_of_clusters}.sav')
 
@@ -171,21 +168,6 @@ def unsupervised_ood_detection(model, logger, args, num_of_clusters, clustering_
         print("Loading weights from saved clustering estimator")
         
     else:
-        train_set, val_set, train_loader, val_loader = mktrainval(args, logger)
-    
-        train_feats_df = []
-
-        dir_path = os.path.join("features", "feats")
-        np.random.seed(1)
-        train_feats1 = np.load("features/imagenet/train/feats.npy")
-        print(train_feats1.shape)
-        train_feats = train_feats1[np.random.choice(train_feats1.shape[0], args.num_of_samples, replace=False)]
-        print(train_feats.shape)
-        train_feats_df.extend(train_feats)
-
-        train_Df = pd.DataFrame(list(zip(train_feats_df)), columns=["feats"])
-        
-        print(len(train_Df))
         estimator.fit(train_feats_df)
         pickle.dump(estimator, open(clustering_model_path_file, 'wb'))
         clusters = estimator.predict(train_feats_df)
@@ -195,6 +177,7 @@ def unsupervised_ood_detection(model, logger, args, num_of_clusters, clustering_
         mean1 = np.load(os.path.join(clustering_model_path, f'estimator_cluster_{num_of_clusters}_gmm_means.npy'), allow_pickle=True)
         precision = np.load(os.path.join(clustering_model_path, f'estimator_cluster_{num_of_clusters}_gmm_precision.npy'), allow_pickle=True)
         print("Loading mean and precision of the saved model")
+        train_loader = Dataset_ood_test(train_feats) ## dummy... not used later... only added to remove error
     
     else:
         clusters = estimator.predict(train_feats_df)
@@ -226,21 +209,13 @@ def unsupervised_ood_detection(model, logger, args, num_of_clusters, clustering_
     ood_feats = []
     in_feats = []
 
-    in_feats.extend(np.load("features/id_data/feats.npy"))
-    if(ood_path == "features/nas_data_imagenet/bright/feats_adjust_scale_1.0.npy" or ood_path == "features/nas_data_imagenet/gaussian_noise/feats_adjust_scale_1.npy"):
-        ood_path = "features/id_data/feats.npy"
-        print("OOD same as ID")
+    in_feats.extend(np.load(id_path))
+    
     ood_feats.extend(np.load(ood_path))
     in_loader, out_loader = mk_id_ood(ood_feats, in_feats)
-
-    ood_set = Dataset_ood_test(ood_feats)
-    out_loader = torch.utils.data.DataLoader(
-        ood_set, batch_size=64, shuffle=False,
-        num_workers=4, pin_memory=True, drop_last=False)
-
+    
     num_groups = num_of_clusters
-    print("############# NUMBER OF CLUSTERS ############", num_of_clusters)
-    run_eval(logger, model, args, estimator, num_groups, clustering_type, covar_type, use_gmm_stats, in_loader, out_loader, mean, var, device, metric)
+    run_eval(logger, model, args, estimator, num_groups, clustering_type, covar_type, use_gmm_stats, train_loader, in_loader, out_loader, mean, var, device, metric)
 
    
 
@@ -250,14 +225,12 @@ def main(args):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     torch.backends.cudnn.benchmark = True
 
-    logger.info(f"Loading model from {args.model_path}")
-    model = resnetv2.KNOWN_MODELS[args.model](head_size=1000)
-
-    state_dict = torch.load(args.model_path)
-    model.load_state_dict_custom(state_dict['model'])
-
-    model = torch.nn.DataParallel(model)
+    logger.info(f"Loading model ")
+    model = define_model()
+    # model = torch.nn.DataParallel(model)
+    model.load_state_dict(torch.load(os.path.join(args.result_path, "models", "best_{}.pt".format(args.seed))))
     model = model.cuda()
+    model.eval()
 
     clustering_type = "gaussian" # kmeans or gaussian
     covar_type = "full"
@@ -265,17 +238,21 @@ def main(args):
     metric = "mahalanobis"
     print(type(args.num_of_clusters))
     args.num_of_clusters = int(args.num_of_clusters)
-    args.num_of_samples = int(args.num_of_samples)
     print(type(args.num_of_clusters), args.num_of_clusters)
-    unsupervised_ood_detection(model, logger, args, args.num_of_clusters, clustering_type, covar_type, use_gmm_stats, args.ood_feats_path, metric, device)
+    unsupervised_ood_detection(model, logger, args, args.num_of_clusters, clustering_type, covar_type, use_gmm_stats, args.ood_feats_path, args.id_feats_path, metric, device)
 
 
 if __name__ == "__main__":
     parser = arg_parser()
-    parser.add_argument("--datadir", required=True)
-    parser.add_argument('--ood_feats_path', help='path to tuned mahalanobis parameters')
-    parser.add_argument("--train_list", required=True)
-    parser.add_argument("--val_list", required=True)
+    parser.add_argument('--result_path', help='id features path')
+    parser.add_argument('--train_feats_path', help='train features path')
+    parser.add_argument('--ood_feats_path', help='ood features path')
+    parser.add_argument('--id_feats_path', help='id features path')
     parser.add_argument("--num_of_clusters", required=True)
-    parser.add_argument("--num_of_samples", required=True)
+    parser.add_argument('--seed', default=0, type=int, help='set seed')
+    parser.add_argument('--gmm_random_state', default=0, type=int, help='gmm random state')
+    parser.add_argument("--clustering_model_path", required=True)
+    parser.add_argument("--adjust_type", required=True)
+    parser.add_argument("--adjust_scale", required=True)
+    
     main(parser.parse_args())
